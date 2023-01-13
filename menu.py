478360e8c42530465b917f0e943f9258bad02eab
rudrabengali <99201733+rudrabengali@users.noreply.github.com>
2023-01-13 14:28:44 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -18,7 +18,7 @@ import client_single
 import client_multi
 # Constants and global variables
 FPS = 60
-WINDOW_SIZE = (1280, 720)jdfal
+WINDOW_SIZE = (1280, 720)
 
 sound: Optional['pygame_menu.sound.Sound'] = None
 surface: Optional['pygame.Surface'] = None
